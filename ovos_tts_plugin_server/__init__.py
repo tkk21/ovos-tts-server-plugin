43f44f64a1ced28f93dd6a633fcea2e4d00e5c2e
jarbasai <jarbasai@mailfence.com>
2022-10-18 01:20:31 +0100

    add priority to metadata

--- ovos_tts_plugin_server/__init__.py
+++ ovos_tts_plugin_server/__init__.py
@@ -36,18 +36,19 @@ OVOSServerTTSConfig = {}
 
 # jarbas public demo instances
 jarbas_hosted = {
-    "S.A.M.": ("https://sam.jarbasai.online", "male"),
-    "PicoTTS": ("https://pico.jarbasai.online", "female"),
-    "Glados": ("https://glados.jarbasai.online", "female"),
-    "Alan Pope [Mimic 1]": ("https://mimic.jarbasai.online", "male"),
-    "R2D2": ("https://r2d2.jarbasai.online", "neutral")
+    "S.A.M.": ("https://sam.jarbasai.online", "male", 100),
+    "PicoTTS": ("https://pico.jarbasai.online", "female", 90),
+    "Glados": ("https://glados.jarbasai.online", "female", 60),
+    "Alan Pope [Mimic 1]": ("https://mimic.jarbasai.online", "male", 80),
+    "R2D2": ("https://r2d2.jarbasai.online", "neutral", 95)
 }
 
 OVOSServerTTSConfig["en-us"] = [
     {"lang": "en-us",
      "url": url,
      "gender": gender,
+     "priority": prio,
      "display_name": f"{display_name} (jarbasai.online)",
      "offline": False}
-    for display_name, (url, gender) in jarbas_hosted.items()
+    for display_name, (url, gender, prio) in jarbas_hosted.items()
 ]
